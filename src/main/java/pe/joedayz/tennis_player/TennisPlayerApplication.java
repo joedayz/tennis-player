74ce9708b3b78084054238c3a52107b965dfc5ce
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2024-11-14 19:07:55 -0500

    first version

---
+++ src/main/java/pe/joedayz/tennis_player/TennisPlayerApplication.java
@@ -0,0 +1,13 @@
+package pe.joedayz.tennis_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/joedayz/tennis_player/TennisPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.tennis_player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
